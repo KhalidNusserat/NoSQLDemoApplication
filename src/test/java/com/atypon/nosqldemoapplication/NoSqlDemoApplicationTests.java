6850b393efb0280cbde68308a6eb9451c100660c
Khalid <KhMKhNu@gmail.com>
2022-06-28 18:36:21 +0300

    Initial commit

---
+++ src/test/java/com/atypon/nosqldemoapplication/NoSqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atypon.nosqldemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
